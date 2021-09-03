7ce084ffaa80a76767a9041a204d6f8e9472ac0e
Corky Maigre <corky.maigre@devoteam.com>
2021-09-03 12:01:21 +0200

    Add job parameters

--- src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
+++ src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
@@ -2,12 +2,9 @@ package com.linkedin.batch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -31,7 +28,9 @@ public class LinkedinBatchApplication {
     @Bean
     public Step packageItemStep() {
         return this.stepBuilderFactory.get("packageItemStep").tasklet((stepContribution, chunkContext) -> {
-            System.out.println("The item has been packaged.");
+            String item = chunkContext.getStepContext().getJobParameters().get("item").toString();
+            String date = chunkContext.getStepContext().getJobParameters().get("run.date").toString();
+            System.out.printf("The %s has been packaged on %s.%n", item, date);
             return RepeatStatus.FINISHED;
         }).build();
     }
